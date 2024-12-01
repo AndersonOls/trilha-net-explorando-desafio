d9a903318e73a1d6789b1cc1fdceaa5eca695c63
Anderson <anderson.silva1996@hotmail.com>
2024-12-01 16:42:51 -0300

    Impl dos TODOs

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,14 +17,15 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
-            {
+            { 
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                
+                throw new Exception("Capacidade máxima de hóspedes excedida.");
             }
         }
 
@@ -37,21 +38,28 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
+            if (Hospedes != null)
+            {
+                return Hospedes.Count;
+            }
             return 0;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Verifica se a suíte e os dias reservados estão devidamente configurados
+            if (Suite == null || DiasReservados <= 0)
             {
-                valor = 0;
+                throw new InvalidOperationException("Suite ou DiasReservados não estão configurados corretamente.");
+            }
+
+            // Cálculo básico: DiasReservados X Suite.ValorDiaria
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Aplicação do desconto de 10% para 10 ou mais dias reservados
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.9m; // Aplica o desconto (90% do valor original)
             }
 
             return valor;
